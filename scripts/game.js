4d297e3c5fbeb49553e79f69ed690a6f112356aa
moorbeton <iveczko@gmail.com>
2017-08-27 18:50:08 +0200

    no message

---
+++ scripts/game.js
@@ -0,0 +1,20 @@
+﻿jewel.game = (function() {
+    var dom = jewel.dom,
+        $ = dom.$;
+
+    /* Ukrywa aktywny ekran (jeśli taki został wybrany) i wyświetla ekran 
+     * o określonym identyfikatorze. */ 
+    function showScreen(screenId) {
+        var activeScreen = $("#game .screen.active")[0],
+            screen = $("#" + screenId)[0];
+        if (activeScreen) {
+            dom.removeClass(activeScreen, "active");
+        }
+        dom.addClass(screen, "active");
+    }
+
+    // Odsłania metody publiczne. 
+    return {
+        showScreen : showScreen
+    };
+})();
